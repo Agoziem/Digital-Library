cdecdab85413590fca19227a954c862d4640a5f9
Agoziem <chiagoziendukwe@gmail.com>
2023-01-10 16:17:48 -0800

    changes

--- Elibrary/settings.py
+++ Elibrary/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['127.0.0.1','neusalibrary.herokuapp.com','www.neusa-elibrary.com','neusa-elibrary.com']
+ALLOWED_HOSTS = ['127.0.0.1','neusalibrary.herokuapp.com','www.nuesae-library.com','nuesae-library.com']
 
 
 # Application definition
