1e9b6a46348eea35920637f7b03f880cc55894d9
Agoziem <chiagoziendukwe@gmail.com>
2023-03-07 01:24:36 -0500

    Changes

--- Elibrary/settings.py
+++ Elibrary/settings.py
@@ -88,12 +88,24 @@ WSGI_APPLICATION = 'Elibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
+
 
 
 import dj_database_url
@@ -140,23 +152,25 @@ USE_TZ = True
 # MEDIA_URL= '/media/'
 # MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
+AWS_S3_SIGNATURE_NAME = config('AWS_S3_SIGNATURE_NAME', default='')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='')
 AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS={'CacheControl':'max-age=86400'}
-AWS_DEFAULT_ACL = config('AWS_DEFAULT_ACL', default='')
+# STATICFILES_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE='Elibrary.storages.MediaStore'
 
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 AWS_LOCATION = 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets"),]
 STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
-STATICFILES_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE='Elibrary.storages.MediaStore'
 
-
-if os.getcwd() == '/app':
-	SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDING_PROTO','https')
-	SECURE_SSL_REDIRECT=True
+# if os.getcwd() == '/app':
+# 	SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDING_PROTO','https')
+# 	SECURE_SSL_REDIRECT=True
 
 JAZZMIN_SETTINGS = {
     "site_logo": "img/Nuesa Logo.png",
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Elibrary.wsgi 
